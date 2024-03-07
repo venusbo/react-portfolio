d1a4108608caa0aa82d82519bb5dc1da2c48aef9
venusbo <3footninja101@gmail.com>
2024-03-07 11:53:12 +1100

    added animations to hero elements and update header and color scheme

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,19 +1,53 @@
 import "./hero.scss"
+import {motion} from "framer-motion";
+
+const textVariants = {
+  initial: {
+    x: -500,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration:1,
+      staggerChildren: 0.1
+
+    }
+  }
+}
 
 const Hero = () => {
   return (
     <div className="hero">
         <div className="wrapper">
-            <div className="textContainer">
-                <h2>John Kim</h2>
-                <h1>Front-End Developer</h1>
-                <div className="buttons">
-                    <button>latest project</button>
-                    <button>contact me</button>
-                </div>
-            </div>
-        </div>
-    </div>
+            <motion.div
+              className="textContainer" 
+              variants={textVariants} 
+              initial="initial" 
+              animate="animate"
+           >
+              <motion.h2 variants={textVariants}>John Kim</motion.h2>
+              <motion.h1 variants={textVariants}>
+                Web Developer + UI/UX Designer
+              </motion.h1>
+
+
+                <motion.div variants = {textVariants} className="buttons">
+                  <motion.button variants = {textVariants}>
+                    latest project
+                  </motion.button>
+                  <motion.button variants = {textVariants}>
+                    Contact me
+                  </motion.button>
+                  <motion.button variants = {textVariants}>
+                    More about me
+                  </motion.button>
+                </motion.div>
+              </motion.div>
+
+          </div>
+      </div>
   );
 };
 
