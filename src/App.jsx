bc20dfb12a0bb52f18a89532336d211257d48a34
venusbo <3footninja101@gmail.com>
2024-03-07 16:21:13 +1100

    capitalised parallax.jsx file name

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./app.scss"
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
-import Parallax from "./components/parallax/parallax";
+import Parallax from "./components/parallax/Parallax";
 
 const App = () => {
   return <div>
