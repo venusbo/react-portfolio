68ee15868e043f7a8cc30f592d93c450bb2ed1c6
venusbo <3footninja101@gmail.com>
2024-03-07 15:08:29 +1100

    renaming Parallax.jsx -> parallax.jsx for vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./app.scss"
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
-import Parallax from "./components/parallax/Parallax";
+import Parallax from "./components/parallax/parallax";
 
 const App = () => {
   return <div>
