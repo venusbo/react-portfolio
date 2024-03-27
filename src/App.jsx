9022acc9a9b824b42778b1430d88c883be132b6c
venusbo <3footninja101@gmail.com>
2024-03-28 02:48:23 +1100

    changed naming in src/App

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./app.scss"
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
 import Parallax from "./components/parallax/Parallax";
-import Portfolio from "./components/portfolio/portfolio";
+import Portfolio from "./components/portfolio/Portfolio";
 
 const App = () => {
   return <div>
